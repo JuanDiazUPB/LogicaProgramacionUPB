86b1bbbccc6d01b7cb192637d3d9b6848f5463ff
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-03-25 22:30:07 -0500

    Add files via upload

---
+++ Laboratorios/Lab22_2403/Bucles anidados 1.py
@@ -0,0 +1,3 @@
+for a in range(10):  # Bucle externo (a) de 0 a 9
+    for b in range(7):  # Bucle interno (b) de 0 a 6
+        print('a vale', a, 'y b vale', b)
---
+++ Laboratorios/Lab22_2403/Bucles anidados 2.py
@@ -0,0 +1,3 @@
+for a in range(1,4):  # Bucle externo (a) de 1 a 3
+    for b in ('Antonio', 'Jesus', 'Mariana'):  # Bucle interno (b) son tres nombres
+        print('a vale', a,'y b vale', b)
