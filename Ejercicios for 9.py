b86e0b46b8a7a52bbf8ef73a93023775ee035afd
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-04-13 23:56:33 -0500

    Delete Ejercicios for 9.py

--- Ejercicios for 9.py
+++
@@ -1,7 +0,0 @@
-print('Ejercicio 9: Dado un número entero positivo, mostrar su factorial.')
-numero = int(input("Número: "))
-f = 1
-if numero != 0:
-    for i in range(1,numero+1):
-        f = f * i
-print("Factorial:", f)
