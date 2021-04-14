d3aa9da07d2eb2cbdab795253837269b05c3279c
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-04-13 23:57:39 -0500

    Delete Ejercicios for 7.py

--- Ejercicios for 7.py
+++
@@ -1,6 +0,0 @@
-print('Ejercicio 7: Escribir un programa que muestre la sumatoria de todos los números entre el 0 y el 100.')
-sum = 0
-for x in range(101):
-    sum += x
-print("Sumatoria:", sum)
-
