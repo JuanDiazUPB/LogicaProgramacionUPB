cf46dd70f84aa9ea9fdc8f37982d63de349d0113
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-04-13 23:57:48 -0500

    Delete Ejercicios for 4.py

--- Ejercicios for 4.py
+++
@@ -1,4 +0,0 @@
-print('Ejercicio 4: Pedir al usuario que ingrese un número entero positivo e imprimir todos los números correlativos entre el ingresado por el usuario y uno menos del doble del mismo.')
-n = int(input("Número: "))
-for n in range(n, n*2):
-    print(n)
