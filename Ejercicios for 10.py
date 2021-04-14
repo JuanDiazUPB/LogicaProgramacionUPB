c9395b8984b7c44118e0a2072104390b77edf08f
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-04-13 23:57:00 -0500

    Delete Ejercicios for 10.py

--- Ejercicios for 10.py
+++
@@ -1,8 +0,0 @@
-print('Ejercicio 10: Crear un algoritmo que muestre los primeros 10 números de la sucesión de Fibonacci.')
-def fib(n):
-    if n <= 1:
-        return n
-    else:
-        return fib(n - 1) + fib(n - 2)
-for x in range(10):
-    print(fib(x))
